2f4993571b10b3d8bba8364479e9a41b4709cdc4
Sudin Kominfotik JB 04 <graficajati339@gmail.com>
2026-04-28 13:24:41 +0700

    Langkah awal membuat form login (Langkah 1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,24 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:aplikasi_aset_komjb/screen/login.dart'; // sesuaikan dengan lokasi file login Anda
+import 'firebase_options.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+
+  // Inisialisasi Firebase SEBELUM runApp
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyCpJYJFx1qo6sAiFpwSxaNpueBHnQFYC88",
-      authDomain: "aplikasi-aset-kominfo-9482d.firebaseapp.com",
-      projectId: "aplikasi-aset-kominfo-9482d",
-      storageBucket: "aplikasi-aset-kominfo-9482d.firebasestorage.app",
-      messagingSenderId: "735091795901",
-      appId: "1:735091795901:web:611019a538a004166354d4",
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
+      child: const MyApp(),
     ),
   );
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Aplikasi Aset Kominfo JB',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const LoginPage(), // langsung ke halaman login
-    );
-  }
 }
